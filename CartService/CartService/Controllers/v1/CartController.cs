a997b1ae262a38fccf8339501a65f36a2175423c
Camilo Guevara <camilo.guevarag@gmail.com>
2025-11-04 19:53:41 -0500

    Added Cart endpoints v1 and v2

---
+++ CartService/CartService/Controllers/v1/CartController.cs
@@ -0,0 +1,45 @@
+﻿using Asp.Versioning;
+using CartService.Application.Services.Cart;
+using CartService.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CartService.Controllers.v1
+{
+    /// <summary>
+    /// Version 1: Returns a full cart model.
+    /// </summary>
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/cart")]
+    public class CartController : ControllerBase
+    {
+        private readonly ICartService _cartService;
+
+        public CartController(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        /// <summary>Get cart info by key.</summary>
+        [HttpGet("{cartId}")]
+        public IActionResult GetCart(string cartId)
+        {
+            var items = _cartService.GetItems(cartId);
+            return Ok(new { cartId, items });
+        }
+
+        [HttpPost("{cartId}/items")]
+        public IActionResult AddItem(string cartId, [FromBody] CartItem item)
+        {
+            _cartService.AddItem(cartId, item);
+            return Ok();
+        }
+
+        [HttpDelete("{cartId}/items/{itemId}")]
+        public IActionResult DeleteItem(string cartId, int itemId)
+        {
+            _cartService.RemoveItem(cartId, itemId);
+            return Ok();
+        }
+    }
+}
---
+++ CartService/CartService/Controllers/v2/CartController.cs
@@ -0,0 +1,41 @@
+﻿using Asp.Versioning;
+using CartService.Application.Services.Cart;
+using CartService.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CartService.Controllers.v2
+{
+    [ApiController]
+    [ApiVersion("2.0")]
+    [Route("api/v{version:apiVersion}/cart")]
+    public class CartController : ControllerBase
+    {
+        private readonly ICartService _cartService;
+
+        public CartController(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        [HttpGet("{cartId}")]
+        public IActionResult GetCartItems(string cartId)
+        {
+            var items = _cartService.GetItems(cartId);
+            return Ok(items);
+        }
+
+        [HttpPost("{cartId}/items")]
+        public IActionResult AddItem(string cartId, [FromBody] CartItem item)
+        {
+            _cartService.AddItem(cartId, item);
+            return Ok();
+        }
+
+        [HttpDelete("{cartId}/items/{itemId}")]
+        public IActionResult DeleteItem(string cartId, int itemId)
+        {
+            _cartService.RemoveItem(cartId, itemId);
+            return Ok();
+        }
+    }
+}
