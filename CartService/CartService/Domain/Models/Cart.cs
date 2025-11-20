22ad5e04e03d9f1e3036d13ed06aa3f13c4bb46b
camilogg15 <48535774+camilogg15@users.noreply.github.com>
2025-11-19 19:10:23 -0500

    module/restful-webapi/task2 - Cart Service Web Api (#4)
    * Added Cart endpoints v1 and v2
    
    * Added unit test

---
+++ CartService/CartService/Domain/Models/Cart.cs
@@ -0,0 +1,8 @@
+﻿namespace CartService.Domain.Models
+{
+    public class Cart
+    {
+        public string Key { get; set; } = string.Empty;
+        public IEnumerable<CartItem> Items { get; set; }
+    }
+}
